0ad6981514e7e131908abc71281902d150da2f03
Osvaldo Diaz <201723072@tese.edu.mx>
2021-04-12 08:42:53 -0500

    Cambios de los compoenentes y formularios de la pagina

---
+++ src/app/hero.ts
@@ -0,0 +1,8 @@
+export interface Hero{
+    id: number;
+    name: string;
+    edad: number;
+    identidad: string;
+    poder: string;
+    universo: string;
+}
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,24 @@
+import {Hero} from "./hero";
+
+export const HEROES: Hero[]=[
+{id: 11, name: "Superman", edad: 35, identidad:"Clark Kent", 
+poder:"Super Fuerza, velocidad, volar", universo: "DC comics"},
+{id: 12, name: "Batman", edad: 27, identidad:"Bruce Wayne",
+poder:"Es millonario", universo:"DC comics"},
+{id: 13, name: "Hulk", edad: 31, identidad:"Bruce Banner",
+poder:"Super fuerza y resistencia", universo:"Marvel"},
+{id: 14, name: "Ironman", edad: 33, identidad: "Tony Stark",
+poder:"Armadura de combate", universo:"Marvel"},
+{id: 15, name: "Thor", edad: 500, identidad: "No tiene",
+poder: "Lanzar rayos", universo:"Marvel"},
+{id: 16, name: "Spiderman", edad: 19, identidad:"Peter Parker",
+poder: "Sentido aracnido, trepar muros y lanzar telaraña", universo:"Marvel"},
+{id: 17, name: "Black Widow", edad: 26, identidad: "Natasha Romanova",
+poder: "Ninguno es espia", universo:"Marvel"},
+{id: 18, name: "Flash", edad: 29, identidad:"Barry Allen",
+poder: "Super velocidad", universo:"DC comics"},
+{id: 19, name: "Scarlett Witch", edad: 37, identidad: "Wanda Maximoff",
+poder:"Magia", universo:"Marvel"},
+{id: 20, name: "Wonder Woman", edad: 45, identidad: "Diana Prince",
+poder: "Super fuerza, volar, armas divinas", universo:"DC comics"}
+]
